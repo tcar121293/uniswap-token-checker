c8ae2c24ced30112640d75cccd71d6a386d1667e
tcar <tihomir.car@uniqcast.com>
2021-02-11 16:53:48 +0100

    add uniswap pair interface;
    Signed-off-by: tcar <tihomir.car@uniqcast.com>

---
+++ src/interfaces/IUniswap.ts
@@ -0,0 +1,17 @@
+
+export interface IUniswapPair{
+    createdAtTimestamp: number,
+    id: string,
+    token0: {
+        derivedETH: string,
+        id: string,
+        name: string,
+        symbol: string
+    },
+    token1: {
+        derivedETH: number,
+        id: string,
+        name: string,
+        symbol: string
+    }
+}
