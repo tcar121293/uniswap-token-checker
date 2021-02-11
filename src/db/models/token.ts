cfb7af1d9f06552cc9ab55dd11f1d4ccb9902f65
tcar <tihomir.car@uniqcast.com>
2021-02-11 14:00:03 +0100

    add token model and init sequelize connection;
    Signed-off-by: tcar <tihomir.car@uniqcast.com>

---
+++ src/db/models/token.ts
@@ -0,0 +1,33 @@
+import { BuildOptions, DataTypes, Model, Sequelize } from "sequelize";
+
+export interface TokenAttributes {
+    id?: number;
+    name: string;
+    timestamp: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+export interface TokenModel extends Model<TokenAttributes>, TokenAttributes {}
+export class Token extends Model<TokenModel, TokenAttributes> {}
+
+export type TokenStatic = typeof Model & {
+    new (values?: object, options?: BuildOptions): TokenModel;
+};
+
+export function TokenFactory (sequelize: Sequelize): TokenStatic {
+    return <TokenStatic>sequelize.define("tokens", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        timestamp: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    })
+}
