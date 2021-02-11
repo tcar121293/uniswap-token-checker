6228f2085b3a1c4b67130535aa22736455cc011e
tcar <tihomir.car@uniqcast.com>
2021-02-11 16:54:32 +0100

    add checker class;
    Signed-off-by: tcar <tihomir.car@uniqcast.com>

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+
+import { Checker } from './lib/Checker'
+import { db, Token } from './db'
+import { UniswapError } from './errors/UniswapError'
+db
+  .sync()
+  .then(() => {
+    main()
+  })
+
+async function main () {
+  const token = await Token.findOne({
+    order: [['timestamp', 'DESC']]
+  })
+
+  const timestamp = token ? token.timestamp : null
+
+  try {
+    const checker = new Checker(timestamp)
+    console.log(checker)
+    setInterval(checker.checkNewToken, Number(process.env.TIME_INTERVAL) || 3000)
+  } catch (err) {
+    if (err instanceof UniswapError) {
+      process.exit(1)
+    }
+    console.error(err)
+  }
+}
---
+++ src/lib/Checker.ts
@@ -0,0 +1,108 @@
+
+import axios from 'axios'
+
+import { Token } from '../db'
+import { IUniswapPair } from '../interfaces/IUniswap'
+import { UniswapError } from '../errors/UniswapError'
+
+export class Checker {
+    private timestamp: number | null
+
+    constructor (timestamp: number | null) {
+      this.timestamp = timestamp
+    }
+
+  public checkNewToken = async () => {
+    const query = this.createQuery(this.timestamp)
+    // should be logger.trace
+    console.log(`Sending request to uniswap with query ${query}`)
+    let data
+    try {
+      data = await axios.post(process.env.UNISWAP_API_URL ||
+        'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2', {
+        query
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    } catch (err) {
+      throw new UniswapError(err.message)
+    }
+
+    if (data && data.data && data.data.data.pairs.length) {
+      // should be logger.debug
+      console.log(`Recieved data ${data.data.data.pairs}`)
+      const { newTokens, currTimestamp } = this.processResult(data.data.data.pairs)
+      // logger.trace
+      console.log(`data to insert ${newTokens}`)
+      await Token.bulkCreate(newTokens)
+      this.timestamp = currTimestamp
+      // logger.trace
+      console.log(`New timestamp: ${this.timestamp}`)
+    }
+  }
+
+public createQuery = (timestamp: number | null): string => {
+  if (timestamp) {
+    return `{
+        pairs(orderBy: createdAtTimestamp, orderDirection: desc, where: {
+            createdAtTimestamp_gt: ${timestamp}
+        }) {
+          id
+          token0 {
+            id
+            symbol
+            name
+            derivedETH
+          }
+          token1 {
+            id
+            symbol
+            name
+            derivedETH
+          }
+         createdAtTimestamp
+        }
+     }`
+  } else {
+    return `{
+        pairs(orderBy: createdAtTimestamp, orderDirection: desc, first: 1) {
+          id
+          token0 {
+            id
+            symbol
+            name
+            derivedETH
+          }
+          token1 {
+            id
+            symbol
+            name
+            derivedETH
+          }
+         createdAtTimestamp
+        }
+     }`
+  }
+}
+
+private processResult (pairs: IUniswapPair[]) {
+  const newTokens: {timestamp: number, name: string}[] = []
+  let currTimestamp: number | null = null
+  for (const pair of pairs) {
+    currTimestamp = pair.createdAtTimestamp
+    newTokens.push({
+      timestamp: pair.createdAtTimestamp,
+      name: pair.token0.name
+    })
+
+    newTokens.push({
+      timestamp: pair.createdAtTimestamp,
+      name: pair.token1.name
+    })
+    currTimestamp = pair.createdAtTimestamp
+  }
+  return { currTimestamp, newTokens }
+}
+}
