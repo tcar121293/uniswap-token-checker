f316ffcc6ef6769498ddd74298ab13b8a81bab49
tcar <tihomir.car@uniqcast.com>
2021-02-11 16:53:21 +0100

    lint code;
    Signed-off-by: tcar <tihomir.car@uniqcast.com>

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,21 +1,21 @@
-import * as sequelize from "sequelize";
-import {TokenFactory} from "./models/token";
+import * as sequelize from 'sequelize'
+import { TokenFactory } from './models/token'
 
 export const db = new sequelize.Sequelize(
-    (process.env.DB_NAME || "nodefactoryDB"),
-    (process.env.DB_USER || "postgres"),
-    (process.env.DB_PASSWORD || "postgres"),
-    {
-        port: Number(process.env.DB_PORT) || 5432,
-        host: process.env.DB_HOST || "localhost",
-        dialect: "postgres",
-        pool: {
-            min: 0,
-            max: 5,
-            acquire: 30000,
-            idle: 10000,
-        },
+  (process.env.DB_NAME || 'nodefactoryDB'),
+  (process.env.DB_USER || 'postgres'),
+  (process.env.DB_PASSWORD || 'postgres'),
+  {
+    port: Number(process.env.DB_PORT) || 5432,
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    pool: {
+      min: 0,
+      max: 5,
+      acquire: 30000,
+      idle: 10000
     }
-);
+  }
+)
 
-export const Token = TokenFactory(db);
+export const Token = TokenFactory(db)
